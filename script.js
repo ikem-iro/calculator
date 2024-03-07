bb89cd56bb936434cf4b5c805c178a154ed4cbad
miraclemenikelechi <52224580+miraclemenikelechi@users.noreply.github.com>
2024-03-07 22:41:03 +0100

    function to toggle calculator on and off

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+const displayScreen = document.querySelector(`#calculator-display`);
+const allClearButton = document.querySelector(`#all-clear`);
+const powerButton = document.querySelector(`#power`);
+const powerButtonText = document.querySelector(`#power span`);
+
+const allButtonsExceptPower = document.querySelectorAll(
+	`button[type=button]:not(#power)`,
+);
+
+function toggleCalculatorPower() {
+	allButtonsExceptPower.forEach((button) => {
+		button.disabled = !button.disabled;
+	});
+
+	powerButtonText.textContent =
+		powerButtonText.textContent === `on` ? `off` : `on`;
+
+	displayScreen.value = "";
+	displayScreen.placeholder = powerButtonText.textContent === `on` ? 0 : ``;
+}
+
+powerButton.addEventListener(`click`, toggleCalculatorPower);
