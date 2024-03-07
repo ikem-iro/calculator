862f2c29fec83870ce7279d0b9bd8dc2d282c514
Chibuikem Iroaganachi <cliroaganachi@gmail.com>
2024-03-07 17:21:41 +0100

    build styling for calculator

---
+++ script.js
@@ -0,0 +1,16 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+
+const operationButtons = document.querySelectorAll('[data-operation]')
+
+const equalsButton = document.querySelector('[data-equals]')
+
+const deleteButton = document.querySelector('[data-delete]')
+
+const allClearButton = document.querySelector('[data-all-clear]')
+
+const onOff = document.querySelector('[data-off-on]');
+
+const prevOperationTextElement = document.querySelector("[data-prev-operation]");
+
+const currOperationTextElement = document.querySelector("[data-curr-operation]");
+
